1ee7f47beeab288f9577d462bb3937c52d2423a0
Jairo <montoysjairito@gmail.com>
2026-07-23 23:41:20 -0600

    mm

--- src/laboratorio/FrmEditorTexto.java
+++ src/laboratorio/FrmEditorTexto.java
@@ -16,7 +16,7 @@ import javax.swing.filechooser.FileNameExtensionFilter;
 import javax.swing.JOptionPane;
 
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Clllick nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
 
